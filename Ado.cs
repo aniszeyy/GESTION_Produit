4e6014a45ea779c30f2a8be430213c32b3d914f4
aniszeyy <76620853+aniszeyy@users.noreply.github.com>
2026-02-25 14:01:05 +0100

    initial commit

---
+++ Ado.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesBDD
+{
+    internal class Ado
+    {
+        // Chaîne de connexion
+        private const string CONNECTIONSTRING = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=GestProduit;Integrated Security=True";
+
+        // a- Objet connection de type SqlConnection
+        public SqlConnection connection;
+
+        // b- Objet de type SqlCommand
+        public SqlCommand commande;
+
+        // c- Objet reader de type SqlDataReader
+        public SqlDataReader reader;
+
+        // d- Objet table de type DataTable
+        public DataTable table;
+
+        // e- Objet dataset de type DataSet
+        public DataSet dataset;
+
+        // f- Objet dataAdapter de type SqlDataAdapter
+        public SqlDataAdapter dataAdapter;
+
+        // g- Objet ligne de type DataRow
+        public DataRow ligne;
+
+        // h- Objet commandeBuilder de type SqlCommandBuilder
+        public SqlCommandBuilder commandeBuilder;
+
+        // Constructeur
+        public Ado()
+        {
+            // Initialisation des objets avec la chaîne de connexion
+            connection = new SqlConnection(CONNECTIONSTRING);
+            commande = new SqlCommand();
+            table = new DataTable();
+            dataset = new DataSet();
+            dataAdapter = new SqlDataAdapter();
+        }
+
+        // Propriétés pour accéder aux objets depuis l'extérieur de la classe
+        public SqlConnection Connection
+        {
+            get { return connection; }
+            set { connection = value; }
+        }
+
+        public SqlCommand Commande
+        {
+            get { return commande; }
+            set { commande = value; }
+        }
+
+        public SqlDataReader Reader
+        {
+            get { return reader; }
+            set { reader = value; }
+        }
+
+        public DataTable Table
+        {
+            get { return table; }
+            set { table = value; }
+        }
+
+        public DataSet Dataset
+        {
+            get { return dataset; }
+            set { dataset = value; }
+        }
+
+        public SqlDataAdapter DataAdapter
+        {
+            get { return dataAdapter; }
+            set { dataAdapter = value; }
+        }
+
+        public DataRow Ligne
+        {
+            get { return ligne; }
+            set { ligne = value; }
+        }
+
+        public SqlCommandBuilder CommandeBuilder
+        {
+            get { return commandeBuilder; }
+            set { commandeBuilder = value; }
+        }
+
+        // Méthode pour établir la connexion
+        public void OuvrirConnexion()
+        {
+            try
+            {
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erreur lors de l'ouverture de la connexion : " + ex.Message);
+            }
+        }
+
+        // Méthode pour fermer la connexion
+        public void FermerConnexion()
+        {
+            try
+            {
+                if (connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erreur lors de la fermeture de la connexion : " + ex.Message);
+            }
+        }
+
+        // Méthode pour disposer des ressources
+        public void Dispose()
+        {
+            reader?.Close();
+            reader?.Dispose();
+            commande?.Dispose();
+            dataAdapter?.Dispose();
+            commandeBuilder?.Dispose();
+            connection?.Close();
+            connection?.Dispose();
+        }
+    }
+}
---
+++ miseAJourProd.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace AccesBDD
+{
+    partial class miseAJourProd
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(miseAJourProd));
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            comboBox1 = new ComboBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(255, 255, 128);
+            button1.Location = new Point(60, 303);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Ajouter";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Red;
+            button2.ForeColor = SystemColors.ButtonFace;
+            button2.Location = new Point(188, 303);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 1;
+            button2.Text = "Supprimer";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(255, 128, 0);
+            button3.Location = new Point(60, 349);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 2;
+            button3.Text = "Modifier";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(0, 64, 0);
+            button4.ForeColor = SystemColors.ButtonFace;
+            button4.Location = new Point(188, 349);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 29);
+            button4.TabIndex = 3;
+            button4.Text = "Afficher";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(667, 349);
+            button5.Name = "button5";
+            button5.Size = new Size(121, 29);
+            button5.TabIndex = 4;
+            button5.Text = "Enrengistrer";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.FromArgb(0, 64, 0);
+            button6.FlatAppearance.BorderColor = Color.Red;
+            button6.FlatAppearance.MouseDownBackColor = Color.FromArgb(192, 0, 0);
+            button6.FlatAppearance.MouseOverBackColor = Color.FromArgb(128, 64, 0);
+            button6.ForeColor = SystemColors.ButtonFace;
+            button6.Location = new Point(1105, 426);
+            button6.Name = "button6";
+            button6.Size = new Size(94, 43);
+            button6.TabIndex = 5;
+            button6.Text = "Quitter";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(382, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(802, 290);
+            dataGridView1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Rockwell Condensed", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 56);
+            label1.Name = "label1";
+            label1.Size = new Size(126, 24);
+            label1.TabIndex = 7;
+            label1.Text = "Référence Produit";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Rockwell Condensed", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(12, 111);
+            label2.Name = "label2";
+            label2.Size = new Size(84, 24);
+            label2.TabIndex = 8;
+            label2.Text = "Designation";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Rockwell Condensed", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(12, 170);
+            label3.Name = "label3";
+            label3.Size = new Size(116, 24);
+            label3.TabIndex = 9;
+            label3.Text = "Quantité Produit";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Rockwell Condensed", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(12, 221);
+            label4.Name = "label4";
+            label4.Size = new Size(102, 24);
+            label4.TabIndex = 10;
+            label4.Text = "Nom Category";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(165, 221);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(188, 28);
+            comboBox1.TabIndex = 11;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(165, 56);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(188, 27);
+            textBox1.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(165, 111);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(188, 27);
+            textBox2.TabIndex = 13;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(165, 167);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(188, 27);
+            textBox3.TabIndex = 14;
+            // 
+            // miseAJourProd
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1211, 481);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(comboBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            DoubleBuffered = true;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "miseAJourProd";
+            Text = "GESTION PRODUITS";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private DataGridView dataGridView1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private ComboBox comboBox1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+    }
+}
